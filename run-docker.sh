6b185ca8ef08865edcbef0bc2294c5c66f2cf50e
nikita1323 <mazhuga.nikita@icloud.com>
2025-05-24 14:40:33 +0300

    added experimental part and research report

--- run-docker.sh
+++ run-docker.sh
@@ -5,8 +5,6 @@ DOCKER_CMD="docker run -it \
     --name $CONTAINER_NAME \
     --net=host \
     --rm \
-    -p 0.0.0.0:6006:6006 \
-    -p 8888:8888 \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v \"$PWD/$PROJECT_NAME:/$PROJECT_NAME\" \
